d603a79b23528726ddfc046f622df454c5bf9bd3
comp500 <comp500@users.noreply.github.com>
2019-04-13 15:11:27 +0100

    SaveParser completed

--- src/SaveParser.ts
+++ src/SaveParser.ts
@@ -1,6 +1,23 @@
-function readString(buffer: ArrayBuffer, offset: number) {
-	let strLength = new DataView(buffer).getUint16(offset);
-	return new TextDecoder("utf-8").decode(buffer.slice(offset + 2, offset + strLength + 2));
+function readString(buffer: ArrayBuffer, offset = new OffsetCounter()) {
+	let offsetNum = offset.get();
+	let strLength = new DataView(buffer).getUint16(offsetNum);
+	offset.increment(strLength + 2);
+	return new TextDecoder("utf-8").decode(buffer.slice(offsetNum + 2, offsetNum + strLength + 2));
+}
+
+class OffsetCounter {
+	private offset = 0;
+	increment(value = 1) {
+		this.offset += value;
+	}
+	get() {
+		return this.offset;
+	}
+	getAndIncrement(value = 1) {
+		let origOffset = this.offset;
+		this.offset += value;
+		return origOffset;
+	}
 }
 
 class Place {
@@ -15,29 +32,25 @@ class Place {
 		public down: number
 	) {}
 
-	static parse(buffer: ArrayBuffer) {
+	static parse(buffer: ArrayBuffer, offset = new OffsetCounter()) {
 		let view = new DataView(buffer);
-		let numPlaces = view.getInt32(0);
+		let numPlaces = view.getInt32(offset.getAndIncrement(4));
 		let places: Place[] = [];
 
-		let offset = 4;
 		for (let i = 0; i < numPlaces; i++) {
-			let id = view.getInt32(offset);
-			offset += 4;
+			let id = view.getInt32(offset.getAndIncrement(4));
 			let desc = readString(buffer, offset);
-			offset += desc.length + 2;
 
 			places[i] = new Place(
 				desc,
 				id,
-				view.getInt32(offset),
-				view.getInt32(offset + 4),
-				view.getInt32(offset + 8),
-				view.getInt32(offset + 12),
-				view.getInt32(offset + 16),
-				view.getInt32(offset + 20),
+				view.getInt32(offset.getAndIncrement(4)),
+				view.getInt32(offset.getAndIncrement(4)),
+				view.getInt32(offset.getAndIncrement(4)),
+				view.getInt32(offset.getAndIncrement(4)),
+				view.getInt32(offset.getAndIncrement(4)),
+				view.getInt32(offset.getAndIncrement(4))
 			);
-			offset += 24;
 		}
 
 		return places;
@@ -46,6 +59,22 @@ class Place {
 
 class Character {
 	constructor(public name: string, public description: string, public id: number, public currentLocation: number) {}
+
+	static parse(buffer: ArrayBuffer, offset = new OffsetCounter()) {
+		let view = new DataView(buffer);
+		let numChars = view.getInt32(offset.getAndIncrement(4));
+		let characters: Character[] = [];
+
+		for (let i = 0; i < numChars; i++) {
+			let id = view.getInt32(offset.getAndIncrement(4));
+			let name = readString(buffer, offset);
+			let desc = readString(buffer, offset);
+
+			characters[i] = new Character(name, desc, id, view.getInt32(offset.getAndIncrement(4)));
+		}
+
+		return characters;
+	}
 }
 
 class Item {
@@ -58,6 +87,38 @@ class Item {
 		public commands: string,
 		public results: string
 	) {}
+
+	static parse(buffer: ArrayBuffer, offset = new OffsetCounter()) {
+		let view = new DataView(buffer);
+		let numItems = view.getInt32(offset.getAndIncrement(4));
+		let items: Item[] = [];
+
+		for (let i = 0; i < numItems; i++) {
+			let id = view.getInt32(offset.getAndIncrement(4));
+			let desc = readString(buffer, offset);
+			let status = readString(buffer, offset);
+			let location = view.getInt32(offset.getAndIncrement(4));
+
+			items[i] = new Item(
+				id,
+				location,
+				desc,
+				status,
+				readString(buffer, offset),
+				readString(buffer, offset),
+				readString(buffer, offset)
+			);
+		}
+
+		return items;
+	}
 }
 
-export default class SaveParser {}
+class Save {
+	constructor(public characters: Character[], public places: Place[], public items: Item[]) {}
+}
+
+export default function parse(buffer: ArrayBuffer) {
+	let offset = new OffsetCounter();
+	return new Save(Character.parse(buffer, offset), Place.parse(buffer, offset), Item.parse(buffer, offset));
+}
