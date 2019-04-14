52e68b1a9a1566f108473ed5daeac8278ba1a2be
comp500 <comp500@users.noreply.github.com>
2019-04-14 01:13:03 +0100

    fun

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,23 @@
 import SaveParser from "./SaveParser";
 import { html, render, TemplateResult } from "lit-html";
 
-render(html`test`, document.getElementById("hi"));
+if (File && FileReader && FileList && Blob) {
+	// Great success! All the File APIs are supported.
+} else {
+	alert("The File APIs are not fully supported in this browser.");
+	render(
+		html`
+			ohno
+		`,
+		document.getElementById("error")
+	);
+}
+
+render(
+	html`
+		test
+	`,
+	document.getElementById("error")
+);
 
 console.log(SaveParser(new ArrayBuffer(100)));
